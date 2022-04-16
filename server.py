17b75cbc5ea5e125464f0158ffcb92af536834dc
Sanmati Choudhary <sanmatichoudhary@dyn-209-2-237-158.dyn.columbia.edu>
2022-04-16 16:31:41 -0400

    fixed the state changes

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
-current_id = 10
 
 lessons = [
 {
@@ -29,20 +28,19 @@ lessons = [
     "lesson_id": "5",
     "next_lesson":"end"
     },
-     
 ]
 
-quiz = [
-{
+quizes = [
+ {
     "quiz_id": "1",
     "next_question":"2"
     },
 
-    {
+   {
     "quiz_id": "2",
     "next_question":"3"
     },
-     {
+  {
     "quiz_id": "3",
     "next_question":"4"
     },
@@ -51,7 +49,7 @@ quiz = [
     "quiz_id": "4",
     "next_question":"5"
     },
-     {
+    {
     "quiz_id": "5",
     "next_question":"end"
     },   
@@ -59,18 +57,26 @@ quiz = [
 # ROUTES
 @app.route('/')
 def welcome():
-    return render_template('home.html')   
+    return render_template('layout.html')   
  
 
-@app.route('/lesson/<lesson_id>')
+@app.route('/learn/<lesson_id>')
 def learn(lesson_id):
-    lesson=lessons[lesson_id]
-    return render_template('lesson.html', lesson=lesson)  
+    lesson=dict()
+    for item in lessons:
+        if item.get("lesson_id") == lesson_id:
+            lesson = item
+            break
+    return render_template('learn.html', lesson=lesson)  
 
 @app.route('/quiz/<quiz_id>')
 def quiz(quiz_id):
-    question=quiz[quiz_id]
-    return render_template('lesson.html', question=question)  
+    question=dict()
+    for item in quizes:
+        if item.get("quiz_id") == quiz_id:
+            question = item
+            break
+    return render_template('quiz.html', question=question)  
 
 if __name__ == '__main__':
    app.run(debug = True)
